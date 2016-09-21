194ee6b65d079fbc5c7932b44313291c01718440
Nadiia Shevtsova <nadya.shevtsova@gmail.com>
2016-09-21 20:48:51 +1000

    Demmo Handeling Events

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -18,23 +18,34 @@ eventsApp.controller('EventController',
           creatorName: 'Bob Smith',
           duration: '1 hr',
           level: 'Advanced',
-          abstract: 'Inthis session you will learn the ins and outs of directives!'
+          abstract: 'Inthis session you will learn the ins and outs of directives!',
+          upVoteCount: 5
         },
         {
           name: 'Scope for fun and profit',
           creatorName: 'John Doe',
           duration: '30 mins',
           level: 'Introductory',
-          abstract: 'This session will take a closer look at scopes. Learn what they do, how they to it, and how to get them to do it for you.'
+          abstract: 'This session will take a closer look at scopes. Learn what they do, how they to it, and how to get them to do it for you.',
+          upVoteCount: 0
         },
         {
           name: 'Well Behaved Controllers',
           creatorName: 'John Doe',
           duration: '2 hours',
           level: 'Intermediate',
-          abstract: 'Controllers are the beginning of everything Angular does. Learn how to craft controllers that will win'
+          abstract: 'Controllers are the beginning of everything Angular does. Learn how to craft controllers that will win the respect of your friends and neighbors.',
+          upVoteCount: 0
         },
       ]
+    }
+
+    $scope.upVoteCount = function(session) {
+      session.upVoteCount++;
+    };
+
+    $scope.downVoteCount = function(session) {
+      session.upVoteCount--;
     };
   }
 );
