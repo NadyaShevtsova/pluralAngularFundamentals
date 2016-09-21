0226d4807f0aac516415ccf03b2ac62fc7dfee25
Nadiia Shevtsova <nadya.shevtsova@gmail.com>
2016-09-21 21:41:34 +1000

    Other Directives Part 1

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -2,6 +2,11 @@
 
 eventsApp.controller('EventController',
   function EventController($scope) {
+
+    $scope.snippet = '<span stype="color:red">hi there</span>';
+    $scope.boolValue = false;
+    $scope.mystyle = {color: 'red'};
+    $scope.myclass = "blue";
     $scope.event = {
       name: 'Angular Boot Camp',
       date: '1/1/2013',
