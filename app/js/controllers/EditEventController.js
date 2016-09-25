b637197f3a73b2610dd9624fcf00b911951df619
Nadiia Shevtsova <nadya.shevtsova@gmail.com>
2016-09-25 17:45:20 +1000

    Validation

--- app/js/controllers/EditEventController.js
+++ app/js/controllers/EditEventController.js
@@ -1,8 +1,11 @@
 'use strict';
 
 eventsApp.controller('EditEventController', function EditEventController($scope) {
-  $scope.saveEvent = function(event) {
-    window.alert('event' + event.name + ' saved!');
+  $scope.saveEvent = function(event, newEventForm) {
+    console.log(newEventForm);
+    if (newEventForm.$valid) {
+      window.alert('event' + event.name + ' saved!');
+    }
   };
 
   $scope.cancelEdit = function() {
